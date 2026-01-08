e0c82121cf7f8d2a7349916f91f8fa54a053f7fd
alirezarmj <a.rashidi.m78@gmail.com>
2026-01-08 19:25:46 +0330

    refactored

--- index.js
+++ index.js
@@ -4,36 +4,74 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 
 const app = express();
-
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Socket server is running");
+});
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // <-- change this to your frontend's URL
+    origin: "*", // or your Vercel URL
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(`A user connected with id ${socket.id}`);
+  console.log("User connected:", socket.id);
 
   socket.on("join_room", (roomId) => {
-    // console.log(`A user with ID: ${socket.id} joined room ${roomId}`);
     socket.join(roomId);
   });
 
   socket.on("send_message", (data) => {
-    // console.log(`A user with ID: ${socket.id} sent a message: ${data.message}`);
     socket.to(data.room).emit("receive_message", data);
   });
-
-  socket.on("disconnect", () => {
-    console.log("A User Disconnected");
-  });
 });
 
-server.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log("Server running on", PORT);
 });
+
+// const http = require("http");
+// const express = require("express");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
+
+// const app = express();
+
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173", // <-- change this to your frontend's URL
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`A user connected with id ${socket.id}`);
+
+//   socket.on("join_room", (roomId) => {
+//     // console.log(`A user with ID: ${socket.id} joined room ${roomId}`);
+//     socket.join(roomId);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     // console.log(`A user with ID: ${socket.id} sent a message: ${data.message}`);
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("A User Disconnected");
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("Server is running on port 3001");
+// });
